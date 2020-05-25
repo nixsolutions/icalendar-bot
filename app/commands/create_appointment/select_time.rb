d6e2d9478132e9e487301b5a7d0a20d3c02bd234
keil19 <alekseydanilov19@gmail.com>
2020-05-25 15:28:34 +0300

    feat: add bot states

--- app/commands/create_appointment/select_time.rb
+++ app/commands/create_appointment/select_time.rb
@@ -1,61 +1,52 @@
 # frozen_string_literal: true
-require 'date'
-require 'time'
-require 'pry'
-module Commands::CreateAppointment
-  class Time < Commands::Base
-    private
 
-    CONFDAY_SELECTION_KEYBOARD = inline_keyboard(
-        [
-            button('June 1, Sat', 'schedule', confday: 1),
-            button('June 2, Sun', 'schedule', confday: 2)
-        ]
-    )
+module Commands
+  module CreateAppointment
+    class SelectTime < Commands::Base
+      private
 
-    def day_selection_keyboard
-      binding.pry
-      inline_keyboard(
-        [
-            button("Today #{Time.now.to_date.strftime('%d of %B, %Y')}", 'schedule', day: 2),
-            button("Tomorrow #{Time.now.to_date.next_day.strftime('%d of %B, %Y')}", 'schedule', day: 1)
-        ]
-      )
-    end
-
-    def handle_call(message)
-      send_message(
+      def handle_call(message)
+        send_message(
           chat_id: message.chat.id,
           text: I18n.t('create_appointment.choose_day'),
-          reply_markup: CONFDAY_SELECTION_KEYBOARD
-      )
-    end
+          reply_markup: day_selection_keyboard
+        )
+      end
 
-    def handle_callback(callback, args)
-      date = Date.new(2019, 06, args.fetch('confday'))
-      # talks = repo.by_date(date)
-
-      send_message(
+      def handle_callback(callback, args)
+        date = Date.new(2019, 1, args.fetch('day'))
+        send_message(
           chat_id: callback.message.chat.id,
-          text: talks_message(date),
-          parse_mode: :markdown
-      )
-      send_message(
-          chat_id: callback.message.chat.id,
-          text: 'time:',
-          reply_markup: inline_keyboard(
-              [
-                  button('18:00', 'schedule', confday: 1),
-                  button('19:00', 'schedule', confday: 2)
-              ]
-          )
-      )
-    end
+          text: message(date),
+          parse_mode: :markdown,
+          reply_markup: time_selection_keyboard
+        )
+      end
 
-    def talks_message(confday)
-      <<~MARKDOWN
-        *——— #{confday.strftime('%B %d, %a')} ———*
-      MARKDOWN
+      def time_selection_keyboard
+        inline_keyboard(
+          [
+            button('18:00', 'schedule', day: 1),
+            button('19:00', 'schedule', day: 2)
+          ]
+        )
+      end
+
+      def day_selection_keyboard
+        inline_keyboard(
+          [
+            button("Today #{Time.now.to_date.strftime('%d of %B, %Y')}", 'schedule', day: 2),
+            button("Tomorrow #{Time.now.to_date.next_day.strftime('%d of %B, %Y')}", 'schedule', day: 1)
+          ]
+        )
+      end
+
+      def message(confday)
+        <<~MARKDOWN
+          *——— #{confday.strftime('%B %d, %a')} ———*
+          Time:
+        MARKDOWN
+      end
     end
   end
 end
---
+++ lib/models/users/states.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Users
+  module States
+    START             = 1
+    SELECT_DATE       = 2
+    SELECT_TIME       = 3
+    WRITE_DESCRIPTION = 4
+  end
+end
