35e10cc4ecb6dabdf3b57e9a658fb04f55e7ff05
keil19 <alekseydanilov19@gmail.com>
2020-06-10 19:31:57 +0300

    refactor: add user to command start

--- app/commands/start.rb
+++ app/commands/start.rb
@@ -4,14 +4,14 @@ module Commands
   class Start < Base
     private
 
-    def handle_call(message)
+    def handle_call(message, user)
       send_message(
         chat_id: message.chat.id,
         text: I18n.t('welcome.text'),
         parse_mode: :markdown,
         reply_markup: Keyboards::MainReplyKeyboard.new.call
       )
-      User.set_state(message.from, :start)
+      user.update_state(User::States::START)
     end
   end
 end
