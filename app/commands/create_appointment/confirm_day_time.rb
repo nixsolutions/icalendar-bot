6dd27ba996cf410de72bcb1daf32c0309c3fd06b
keil19 <alekseydanilov19@gmail.com>
2020-06-15 18:35:48 +0300

    refacor: confirm day time command

--- app/commands/create_appointment/confirm_day_time.rb
+++ app/commands/create_appointment/confirm_day_time.rb
@@ -3,23 +3,43 @@
 module Commands
   module CreateAppointment
     class ConfirmDayTime < Commands::Base
+      def self.available_transition
+        Commands::CreateAppointment::WriteSummary
+      end
+
       private
 
-      def handle_callback(callback, args)
-        date_time = DateTime.strptime(args.fetch('day_time'), '%Q')
-        send_message(
-          chat_id: callback.message.chat.id,
-          text: message_text(date_time),
+      def handle_call(message, user)
+        hour, min = message.text.split(':').map(&:to_i)
+        date = user.current_appointment.started_at.change(hour: hour, min: min)
+        send_message(chat_id: message.chat.id, **message(date))
+        save_date(date, user)
+      end
+
+      def handle_callback(callback, user, args)
+        date = DateTime.strptime(args.fetch('day_time'), '%Q')
+        send_message(chat_id: callback.message.chat.id, **message(date))
+
+        save_date(date, user)
+      end
+
+      def message(date)
+        {
+          text: message_text(date),
           parse_mode: :markdown
-        )
-        User.set_state(callback.from, :confirm_day_time)
+        }
       end
 
       def message_text(day_time)
         <<~MARKDOWN
           *——— ⌚️#{day_time.strftime('%b %-d, %H:%M')} ———*
-          #{I18n.t('create_appointment.subject')}
+          #{I18n.t('create_appointment.summary')}
         MARKDOWN
+      end
+
+      def save_date(date, user)
+        user.current_appointment.update_attributes(started_at: date)
+        user.update_state(User::States::CONFIRM_DAY_TIME)
       end
     end
   end
