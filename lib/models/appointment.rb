b89b26d06f6fec50a891ff35d5d52cd349ed1929
keil19 <alekseydanilov19@gmail.com>
2020-06-15 10:42:46 +0300

    feat: default appointment status

--- lib/models/appointment.rb
+++ lib/models/appointment.rb
@@ -3,9 +3,14 @@
 class Appointment
   include Dynamoid::Document
   table capacity_mode: :on_demand
+  STATUS_ENUM = [
+    DRAFT = :draft,
+    COMPLETE = :complete
+  ].freeze
 
   field :subject
   field :description
+  field :status, default: DRAFT
   field :started_at, :datetime
   field :ended_end, :datetime
   field :index_number, :integer
