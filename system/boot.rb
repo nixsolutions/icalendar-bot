138a6c4be4490e20c8e5b6c4e392a6a1b7719b17
keil19 <alekseydanilov19@gmail.com>
2020-06-15 09:41:48 +0300

    config: prelad dynamoid models

--- system/boot.rb
+++ system/boot.rb
@@ -24,11 +24,11 @@ class ICalendarBot
   def self.init
     Zeitwerk::Loader.new.tap do |loader|
       loader.enable_reloading
-      loader.preload File.join('config/initializers')
+      loader.preload File.join('lib')
       loader.push_dir File.join('app')
-      loader.push_dir File.join('lib')
       loader.collapse('lib/services')
       loader.collapse('lib/models')
+      loader.preload File.join('config/initializers')
       loader.setup
     end
   end
